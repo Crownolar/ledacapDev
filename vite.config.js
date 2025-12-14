f148c7da4f843364fab5889b7a37a5694f6561a8
Crownolar <oriadeolamilekan500@gmail.com>
2025-12-14 09:26:12 +0100

    vite updates

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://194-146-38-237.cloud-xip.com",
         changeOrigin: true,
         secure: false,
       },
